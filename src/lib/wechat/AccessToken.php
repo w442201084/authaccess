b926b01f9525f986ce161c0696f5637b0e8f4ef0
wushuang <wushuang@bubugao.com>
2020-12-17 18:53:01 +0800

    优化

--- src/lib/wechat/AccessToken.php
+++ src/lib/wechat/AccessToken.php
@@ -11,6 +11,11 @@ namespace access\lib\wechat;
 use access\config\WeChatServiceConfig;
 use access\lib\common\Common;
 
+/**
+ * @desc 微信用户授权获取access_token获取用户openID (oauth_2.0协议)
+ * Class AccessToken
+ * @package access\lib\wechat
+ */
 class AccessToken
 {
     private $requestUrl = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid=%s&secret=%s&code=%s&grant_type=authorization_code';
@@ -21,13 +26,13 @@ class AccessToken
             WeChatServiceConfig::APP_ID , WeChatServiceConfig::APP_SECRET , $code);
     }
 
-    public function getAccessToken()
+    public function getAccessTokenByCode()
     {
         $handle = new Common();
         $results = $handle -> httpRequest($this -> requestUrl);
         $results = json_decode($results , true);
-        if( !empty($results['access_token']) ) {
-            return $results['access_token'];
+        if(!empty($results['access_token']) ) {
+            return $results;
         } else {
             throw new \Exception("获取access_token失败，{$results['errmsg']}" , 100001);
         }
